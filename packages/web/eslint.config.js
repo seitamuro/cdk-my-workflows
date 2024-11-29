c5c80d2ff9ae42a035e8f1aedf9696cc29f36059
seitamuro <seitamuro0210@gmail.com>
2024-11-30 03:19:45 +0900

    anyの扱いが厳しすぎたので緩和

--- packages/web/eslint.config.js
+++ packages/web/eslint.config.js
@@ -74,6 +74,8 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
     },
   },
   {
