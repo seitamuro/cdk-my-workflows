74f1ff7c49dcd3ea40d75e9402907d6d650faa2e
seitamuro <seitamuro0210@gmail.com>
2024-11-30 03:15:58 +0900

    any型の値の代入を許可

--- packages/web/eslint.config.js
+++ packages/web/eslint.config.js
@@ -72,6 +72,7 @@ export default tseslint.config(
         },
       ],
       '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
     },
   },
   {
