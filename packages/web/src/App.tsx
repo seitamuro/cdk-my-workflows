83213236ef618ab52a80b42b2fee82d1a6295bc0
seitamuro <seitamuro0210@gmail.com>
2024-11-30 10:45:06 +0900

    clineでおかしくなっていた部分を修正

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -7,24 +7,24 @@ import { SignInPage } from '@/pages/SignInPage';
 import { SignOutPage } from '@/pages/SignOutPage';
 import { SignUpPage } from '@/pages/SignUpPage';
 import { UserInfoPage } from '@/pages/UserInfoPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
+import './App.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <NavigationBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/signout" element={<SignOutPage />} />
-          <Route path="/confirm" element={<ConfirmPage />} />
-          <Route path="/user" element={<UserInfoPage />} />
-          <Route path="/buckets" element={<BucketListPage />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signout" element={<SignOutPage />} />
+        <Route path="/confirm" element={<ConfirmPage />} />
+        <Route path="/user-info" element={<UserInfoPage />} />
+        <Route path="/bucket-list" element={<BucketListPage />} />
+      </Routes>
+    </AuthProvider>
   );
 };
 
